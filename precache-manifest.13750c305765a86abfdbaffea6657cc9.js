ebda9dcefc09bfde6f820edabc9fc54d0f1627f7
GaryCameron <gcamuk@me.com>
2019-09-11 15:40:54 +0100

    Updates

---
+++ precache-manifest.13750c305765a86abfdbaffea6657cc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67c832b90ea5b5957fbaf0ba9e12fef",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "5cde6f12f5817e4acce0",
+    "url": "/first-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "af127fdd9f96f244558a",
+    "url": "/first-react-app/static/js/2.5379c1ce.chunk.js"
+  },
+  {
+    "revision": "5cde6f12f5817e4acce0",
+    "url": "/first-react-app/static/js/main.4bc86660.chunk.js"
+  },
+  {
+    "revision": "f7618df7625f3bbc96d3",
+    "url": "/first-react-app/static/js/runtime~main.980d9c78.js"
+  }
+]);
